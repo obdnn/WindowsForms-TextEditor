87b5a73f055a2765f50aebadc38b9c68e421bd32
obdnn <nickita.ukraine@gmail.com>
2026-02-16 14:55:51 +0200

    Initial commit - Windows Forms Text Editor

---
+++ LabTextRedactorWindows Forms App (.NET)/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace LabTextRedactorWindowsFormsApp
+{
+    public partial class Form1 : Form
+    {
+        private string currentFilePath = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // NEW
+        private void newToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            currentFilePath = "";
+            this.Text = "New File - Text Editor";
+        }
+
+        // OPEN
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Text Files (*.txt)|*.txt|All Files (*.*)|*.*";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                currentFilePath = openFileDialog.FileName;
+                textBox1.Text = File.ReadAllText(currentFilePath);
+                this.Text = currentFilePath + " - Text Editor";
+            }
+        }
+
+        // SAVE
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(currentFilePath))
+            {
+                SaveFileDialog saveFileDialog = new SaveFileDialog();
+                saveFileDialog.Filter = "Text Files (*.txt)|*.txt|All Files (*.*)|*.*";
+
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    currentFilePath = saveFileDialog.FileName;
+                }
+                else
+                {
+                    return;
+                }
+            }
+
+            File.WriteAllText(currentFilePath, textBox1.Text);
+            this.Text = currentFilePath + " - Text Editor";
+            MessageBox.Show("File saved successfully!");
+        }
+
+        // EXIT
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
